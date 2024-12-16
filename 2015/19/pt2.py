a526adbecc763c4e0bb3bffa78cc9c2f45f6a94b
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2024-12-15 22:34:15 -0500

    2015 day 19 part 2, kind of works

--- 2015/19/pt2.py
+++ 2015/19/pt2.py
@@ -2,6 +2,7 @@ import re
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+target = lines[-1][:-1]
 
 transforms = {}
 for line in lines[:-2]:
@@ -18,19 +19,37 @@ def transform_molecule(molecule):
         for match in matches:
             for value in transforms[key]:
                 new_molecule = molecule[:match.start()] + value + molecule[match.end():]
-                if new_molecule == 'e' or 'e' not in new_molecule:
+                if new_molecule != 'e' and 'e' in new_molecule:
+                    continue
+                if new_molecule == target or len(new_molecule) < len(target):
                     molecules.add(new_molecule)
     return molecules
 
-molecule = lines[-1][:-1]
-molecule_steps = [set()]
-molecule_steps[0].add(molecule)
-while 'e' not in molecule_steps[-1]:
-    molecules = set()
-    for molecule in molecule_steps[-1]:
-        transformed = transform_molecule(molecule)
-        molecules = molecules.union(transformed)
-    print(len(molecules))
-    molecule_steps.append(molecules)
+def lev(a, b, depth=1):
+    return max(len(a), len(b))
+    # if depth > 10:
+    #     return depth
+    # print('lev', a, b)
+    if len(b) == 0:
+        return len(a)
+    if len(a) == 0:
+        return len(b)
+    if a[0] == b[0]:
+        return lev(a[1:], b[1:], depth)
+    return 1 + min(lev(a[1:], b, depth+1), lev(a, b[1:], depth+1), lev(a[1:], b[1:], depth+1))
 
-print("final", len(molecule_steps) - 1)
+def recurse_molecule(molecule):
+    nexts = transform_molecule(molecule)
+    nexts = sorted(nexts, key=lambda x : lev(x, 'e'))
+    for next in nexts:
+        if next == 'e':
+            return ['e']
+        result = recurse_molecule(next)
+        if len(result) > 0:
+            return [next, *result]
+    return []
+
+result = recurse_molecule(target)
+print(result)
+print(len(result))
+print(result[0] == target)
